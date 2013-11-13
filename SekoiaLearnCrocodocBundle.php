546f384531e0d4ae94c6ffb499398cfe3f6262bb
PrinceAnsong <prince.ansong@gmail.com>
2013-11-13 13:38:48 +0100

    update SekoiaLearnCrocodocBundle

--- SekoiaLearnCrocodocBundle.php
+++ SekoiaLearnCrocodocBundle.php
@@ -2,8 +2,18 @@
 
 namespace SekoiaLearn\CrocodocBundle;
 
+use SekoiaLearn\CrocodocBundle\DependencyInjection\SekoiaLearnCrocodocExtension;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class SekoiaLearnCrocodocBundle extends Bundle{
+class SekoiaLearnCrocodocBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
 
+        // register extensions that do not follow the conventions manually
+        $container->registerExtension(new SekoiaLearnCrocodocExtension());
+    }
 }
